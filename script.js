4fe2f203e4359b4fbf5dbcdf650cf6a49f80e57b
persely <Perselymongwe@gmail.com>
2025-08-02 13:26:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.getElementById("startJourneyBtn").onclick = () => {
 document.getElementById("enrollBtn").onclick = () => {
   alert("Booking feature coming soon. Stay tuned!");
 };
+
+section {
+  opacity: 0;
+  transform: translateY(30px);
+  transition: opacity 0.6s ease, transform 0.6s ease;
+}
+
+section.fade-in {
+  opacity: 1;
+  transform: translateY(0);
+}
