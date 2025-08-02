7863a29bd7c699d67de0662ae32fb0774aa46fef
persely <Perselymongwe@gmail.com>
2025-08-02 13:14:29 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("startJourneyBtn").onclick = () => {
+  document.getElementById("why-maspala").scrollIntoView({ behavior: 'smooth' });
+};
+
+document.getElementById("enrollBtn").onclick = () => {
+  alert("Booking feature coming soon. Stay tuned!");
+};
